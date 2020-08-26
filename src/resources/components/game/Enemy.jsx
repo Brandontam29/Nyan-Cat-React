379b43b34cbe3d434d5d8329f243552e763b4cf8
Brandon Tam <brandon.tam29@gmail.com>
2020-08-26 13:41:45 -0400

    added tail hitbox leniency

--- src/resources/components/game/Enemy.jsx
+++ src/resources/components/game/Enemy.jsx
@@ -55,14 +55,10 @@ const Enemy = ({
     const [top, setTop] = useState(-ENEMY_HEIGHT);
     const [touched, setTouched] = useState(false);
     useEffect(() => {
-        console.log(GAME_HEIGHT - ENEMY_HEIGHT * (2 / 3), top, GAME_HEIGHT - PLAYER_HEIGHT - ENEMY_HEIGHT);
-        console.log(GAME_HEIGHT - ENEMY_HEIGHT * (2 / 3) > top && top > GAME_HEIGHT - PLAYER_HEIGHT - ENEMY_HEIGHT);
-    }, [top]);
-    useEffect(() => {
         let id = 0;
         if (falling && top < GAME_HEIGHT + ENEMY_HEIGHT && !pause) {
             if (!touched
-                && GAME_HEIGHT - ENEMY_HEIGHT * (2 / 3) > top
+                && GAME_HEIGHT - ENEMY_HEIGHT * (1 / 2) > top
                 && top > GAME_HEIGHT - PLAYER_HEIGHT - ENEMY_HEIGHT
                 && playerPosition === spot) {
                 setTouched(true);
